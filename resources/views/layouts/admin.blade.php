7fef27b7dd26555280439995a45d5039c1e46708
aziyan99 <rajaazian08@gmail.com>
2022-04-02 21:46:41 +0700

    add title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title')</title>
+    <x-title />
     <x-favico />
 
     <!-- Google Font: Source Sans Pro -->
