7cf852858caa074a7051aa7598f70b9aeb329e10
mberry87 <berry311287@gmail.com>
2022-04-04 21:51:52 +0700

    update dashboard

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -10,7 +10,58 @@
 
 @section('content')
     <div class="row">
-        <div class="col-12">
+        <div class="col-md-3">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $users }}</h3>
+                    <p>User Terdaftar</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+                <a href="{{ route('user.index') }}" class="small-box-footer">More info <i
+                        class="fas fa-arrow-circle-right"></i></a>
+            </div>
         </div>
+        <div class="col-md-3">
+            <div class="small-box bg-primary">
+                <div class="inner">
+                    <h3>{{ $jenis_kapal }}</h3>
+                    <p>Jenis Kapal</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-android-boat"></i>
+                </div>
+                <a href="{{ route('jenis_kapal.index') }}" class="small-box-footer">More info <i
+                        class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $pelabuhan }}</h3>
+                    <p>Pelabuhan</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-map"></i>
+                </div>
+                <a href="{{ route('pelabuhan.index') }}" class="small-box-footer">More info <i
+                        class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ $terminal }}</h3>
+                    <p>Dermaga / Terminal</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-cube"></i>
+                </div>
+                <a href="{{ route('terminal.index') }}" class="small-box-footer">More info <i
+                        class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    </div>
     </div>
 @endsection
