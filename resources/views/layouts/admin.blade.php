6fd07429ba17b987e98c1c88da251b8d5361e92b
mberry87 <berry311287@gmail.com>
2022-06-15 11:52:57 +0700

    repair

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -78,7 +78,7 @@
                         <li class="user-footer">
                             <a href="{{ route('profile.index') }}" class="btn btn-default btn-flat">Profile</a>
                             <a class="btn btn-default btn-flat float-right" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                          document.getElementById('logout-form').submit();">Sign out</a>
+                                        document.getElementById('logout-form').submit();">Sign out</a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST"
                                 class="d-none">
@@ -127,101 +127,9 @@
                                     </p>
                                 </a>
                             </li>
-                            <li class="nav-header">MENU UTAMA</li>
-                            <li
-                                class="nav-item
-                                        {{ Request::is('admin/tersus/*') ? 'menu-open' : '' }}
-                                        {{ Request::is('admin/bup/*') ? 'menu-open' : '' }}
-                                        {{ Request::is('admin/pelnas/*') ? 'menu-open' : '' }}
-                                        {{ Request::is('admin/keagenan_kapal/*') ? 'menu-open' : '' }}
-                                        {{ Request::is('admin/pbm/*') ? 'menu-open' : '' }}
-                                        {{ Request::is('admin/jpt/*') ? 'menu-open' : '' }}
-                                        {{ Request::is('admin/pelra/*') ? 'menu-open' : '' }}">
-                                <a href="#" class="nav-link">
-                                    <i class="nav-icon fas fa-book"></i>
-                                    <p>
-                                        Form Data
-                                        <i class="fas fa-angle-left right"></i>
-                                    </p>
-                                </a>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item">
-                                        <a href="{{ route('tersus.index') }}"
-                                            class="nav-link {{ Request::is('admin/tersus/*') ? 'active' : '' }}">
-                                            <i class="nav-icon far fa-circle"></i>
-                                            <p>
-                                                Data TERSUS
-                                            </p>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item">
-                                        <a href="{{ route('bup.index') }}"
-                                            class="nav-link {{ Request::is('admin/bup/*') ? 'active' : '' }}">
-                                            <i class="nav-icon far fa-circle"></i>
-                                            <p>
-                                                Data BUP
-                                            </p>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item ">
-                                        <a href="{{ route('pelnas.index') }}"
-                                            class="nav-link {{ Request::is('admin/pelnas/*') ? 'active' : '' }}">
-                                            <i class="nav-icon far fa-circle"></i>
-                                            <p>
-                                                Data PELNAS
-                                            </p>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item ">
-                                        <a href="{{ route('keagenan_kapal.index') }}"
-                                            class="nav-link {{ Request::is('admin/keagenan_kapal/*') ? 'active' : '' }}">
-                                            <i class="nav-icon far fa-circle"></i>
-                                            <p>
-                                                Data KEAGENAN KAPAL
-                                            </p>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item ">
-                                        <a href="{{ route('pbm.index') }}"
-                                            class="nav-link {{ Request::is('admin/pbm/*') ? 'active' : '' }}">
-                                            <i class="nav-icon far fa-circle"></i>
-                                            <p>
-                                                Data PBM
-                                            </p>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item ">
-                                        <a href="{{ route('jpt.index') }}"
-                                            class="nav-link {{ Request::is('admin/jpt/*') ? 'active' : '' }}">
-                                            <i class="nav-icon far fa-circle"></i>
-                                            <p>
-                                                Data JPT
-                                            </p>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item ">
-                                        <a href="{{ route('pelra.index') }}"
-                                            class="nav-link {{ Request::is('admin/pelra/*') ? 'active' : '' }}">
-                                            <i class="nav-icon far fa-circle"></i>
-                                            <p>
-                                                Data PELRA
-                                            </p>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
+
+                            <x-backend.side-menu />
+
                             @if (auth()->user()->role == 'admin')
                                 <li class="nav-header">MENU SISTEM</li>
                                 <li
@@ -385,8 +293,7 @@
     </div>
     <!-- /.content-wrapper -->
     <footer class="main-footer">
-        <small>Copyright &copy; 2022 Kantor Unit Penyelenggaraan Pelabuhan
-            Kelas I | Tanjung Uban <a href="#"></a>.</small>
+        <small>Copyright &copy; 2022 strawBerry | Tg.pinang <a href="#"></a>.</small>
     </footer>
 
     <!-- jQuery -->
