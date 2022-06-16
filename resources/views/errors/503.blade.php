f4a88193131078db61e91ec5e1a99f4649dbd562
mberry87 <berry311287@gmail.com>
2022-06-16 08:38:54 +0700

    maintencane

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('')</title>
+
+    <link rel="stylesheet" href="{{ asset('admin') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('admin') }}/dist/css/adminlte.min.css?v=3.2.0">
+    <script src="{{ asset('admin') }}/plugins/jquery/jquery.min.js"></script>
+</head>
+
+<body>
+    <div class="container text-center p-5">
+        <img src="{{ asset('image') }}/perhubungan.png" alt="logo" style="width: 65px">
+        <h2>Website Sedang Maintenance</h2>
+
+        <section>
+            <p>Kami sedang melakukan perbaikan pada beberapa bagian website, untuk info lebih lanjut silahkan hubungi
+                admin <a href="#">admin@develops.com</a>. Kami akan segera kembali</p>
+            <p>Salam Sehat Selalu</p>
+        </section>
+    </div>
+</body>
+
+<script src="{{ asset('admin') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</html>
